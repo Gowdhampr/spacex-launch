88d22b3d8c6dc5b6050fefe7d80a6952f29e9a27
Gowdham Rajendran <prgowdham@gmail.com>
2021-01-31 20:08:36 +0530

    Fixed history missing dependency warning

--- src/services/spacexLaunchService.js
+++ src/services/spacexLaunchService.js
@@ -6,7 +6,3 @@ import apiClient from "../apiClient";
 export const getSpacexLaunchList = (year = '', launchSuccess = '', landSuccess = '', limit = 100) => {
     return apiClient.get(`/launches?limit=${limit}&launch_success=${launchSuccess}&land_success=${landSuccess}&launch_year=${year}`);
 }
-
-export default {
-    getSpacexLaunchList,
-};
